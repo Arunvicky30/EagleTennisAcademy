b82f850276af2ac229bf68ac149214b79bec93a9
Arun-gembrill <ajeyabalan@gembrill.com>
2026-06-15 17:17:10 +0530

    Fix Vercel SPA routing

--- eagle-tennis-backend/src/db/index.ts
+++ eagle-tennis-backend/src/db/index.ts
@@ -4,11 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // database: process.env.DB_NAME,
+   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
